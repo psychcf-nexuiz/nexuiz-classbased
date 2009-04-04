daeaa61b490b16fcf0cc1d9dcc8f2aa03833e2c1
Will Riley <will@psychdesigns.net>
2009-04-04 17:51:08 -0400

    removed duplicate code in build.sh

--- build.sh
+++ build.sh
@@ -21,26 +21,3 @@ echo "*** Making pk3 ***"
 cd ../../
 zip -r data-svn * -u -1 -x *.svn-base #-i *.dat *.cfg *.lno
 cp data-svn.zip ~/.nexuiz/data/data-svn.pk3
-#!/bin/sh
-
-echo "Building Nexuiz..."
-
-echo "*** MENU ***"
-cd ./data/qcsrc/menu
-#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
-fteqcc
-
-echo "*** CLIENT ***"
-cd ../client
-#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
-fteqcc
-
-echo "*** SERVER ***"
-cd ../server
-#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
-fteqcc
-
-echo "*** Making pk3 ***"
-cd ../../
-zip -r data-svn * -u -1 -x *.svn-base #-i *.dat *.cfg *.lno
-cp data-svn.zip ~/.nexuiz/data/data-svn.pk3
