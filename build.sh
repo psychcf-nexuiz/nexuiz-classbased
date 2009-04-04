b8e8adef42825c8667973b7a76c623365a4ecfad
Will Riley <will@psychdesigns.net>
2009-04-04 16:28:03 -0400

    added classbased changes from local svn checkout

--- build.sh
+++ build.sh
@@ -4,21 +4,43 @@ echo "Building Nexuiz..."
 
 echo "*** MENU ***"
 cd ./data/qcsrc/menu
-~/.nexuiz/Nexuiz_2.5/fteqcc/fteqcc-linux-x86_64
-#fteqcc
+#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
+fteqcc
 
 echo "*** CLIENT ***"
 cd ../client
-~/.nexuiz/Nexuiz_2.5/fteqcc/fteqcc-linux-x86_64
-#fteqcc
+#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
+fteqcc
 
 echo "*** SERVER ***"
 cd ../server
-~/.nexuiz/Nexuiz_2.5/fteqcc/fteqcc-linux-x86_64
-#fteqcc
+#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
+fteqcc
 
 echo "*** Making pk3 ***"
 cd ../../
+zip -r data-svn * -u -1 -x *.svn-base #-i *.dat *.cfg *.lno
+cp data-svn.zip ~/.nexuiz/data/data-svn.pk3
+#!/bin/sh
 
-zip -r ../data-svn * -u -1 -x .git #-i *.dat *.cfg *.lno
-cp ../data-svn.zip ~/.nexuiz/data/data-svn.pk3
+echo "Building Nexuiz..."
+
+echo "*** MENU ***"
+cd ./data/qcsrc/menu
+#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
+fteqcc
+
+echo "*** CLIENT ***"
+cd ../client
+#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
+fteqcc
+
+echo "*** SERVER ***"
+cd ../server
+#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
+fteqcc
+
+echo "*** Making pk3 ***"
+cd ../../
+zip -r data-svn * -u -1 -x *.svn-base #-i *.dat *.cfg *.lno
+cp data-svn.zip ~/.nexuiz/data/data-svn.pk3
