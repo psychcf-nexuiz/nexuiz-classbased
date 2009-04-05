fd30ef955a2aba60959d275af73f7bdb05db0320
Will Riley <will@psychdesigns.net>
2009-04-05 15:45:21 -0400

    more fixes to the mutators menu

--- data/qcsrc/menu/nexuiz/dialog_multiplayer_create_mutators.c
+++ data/qcsrc/menu/nexuiz/dialog_multiplayer_create_mutators.c
@@ -178,6 +178,8 @@ void fillNexuizMutatorsDialog(entity me)
 		me.TD(me, 1, 2, e = makeNexuizCheckBox(0, "g_pinata", "Pinata"));
 	me.TR(me);
 		me.TD(me, 1, 2, e = makeNexuizCheckBoxEx(2, 0, "g_weapon_stay", "Weapons stay"));
+    me.TR(me);
+        me.TD(me, 1, 2, e = makeNexuizCheckBox(0, "g_classbased", "Class based gameplay"));
 	me.TR(me);
 		s = makeNexuizSlider(10, 50, 1, "g_bloodloss");
 		me.TD(me, 1, 2, e = makeNexuizSliderCheckBox(0, 1, s, "Blood loss"));
@@ -229,8 +231,6 @@ void fillNexuizMutatorsDialog(entity me)
 		me.TD(me, 1, 4, e = makeNexuizRadioButton(1, "g_minstagib", string_null, "MinstaGib"));
 	me.TR(me);
 		me.TD(me, 1, 4, e = makeNexuizRadioButton(1, "g_nixnex", string_null, "NixNex"));
-    me.TR(me);
-        me.TD(me, 1, 2, e = makeNexuizRadioButton(1, "g_classbased", string_null, "Class based gameplay"));
 	me.TR(me);
 		me.TDempty(me, 0.2);
 		me.TD(me, 1, 3.8, e = makeNexuizCheckBox(0, "g_nixnex_with_laser", "with laser"));
