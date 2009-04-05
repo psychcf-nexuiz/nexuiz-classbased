4a8557017f4515ac8c4f184baa37c027d5c15dcc
Will Riley <will@psychdesigns.net>
2009-04-05 15:12:42 -0400

    some minor changes to the build script

--- build.sh
+++ build.sh
@@ -4,20 +4,21 @@ echo "Building Nexuiz..."
 
 echo "*** MENU ***"
 cd ./data/qcsrc/menu
-#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
-fteqcc
+~/.nexuiz/Nexuiz_2.5/fteqcc/fteqcc-linux-x86_64
+#fteqcc
 
 echo "*** CLIENT ***"
 cd ../client
-#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
-fteqcc
+~/.nexuiz/Nexuiz_2.5/fteqcc/fteqcc-linux-x86_64
+#fteqcc
 
 echo "*** SERVER ***"
 cd ../server
-#~/.nexuiz/Nexuiz/sources/fteqcc/fteqcc-linux-x86_64
-fteqcc
+~/.nexuiz/Nexuiz_2.5/fteqcc/fteqcc-linux-x86_64
+#fteqcc
 
 echo "*** Making pk3 ***"
 cd ../../
-zip -r data-svn * -u -1 -x *.svn-base #-i *.dat *.cfg *.lno
-cp data-svn.zip ~/.nexuiz/data/data-svn.pk3
+
+zip -r ../data-svn * -u -1 -x .git #-i *.dat *.cfg *.lno
+cp ../data-svn.zip ~/.nexuiz/data/data-svn.pk3
